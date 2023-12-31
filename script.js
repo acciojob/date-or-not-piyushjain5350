21655753a1fcdebacc7d53f9ecd15334b08c67fd
Piyush Jain <126599802+piyushjain5350@users.noreply.github.com>
2023-12-31 22:33:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-var isDate = function (input) {
-  //   write your code here
-};
+function isDate(input) {
+  if (Object.prototype.toString.call(input) === "[object Date]") {
+    // Check if it's a valid date object
+    return !isNaN(input.getTime());
+  } else if (typeof input === 'string') {
+    // Check if it's a valid date string
+    const parsedDate = new Date(input);
+    return !isNaN(parsedDate.getTime());
+  } else {
+    return false;
+  }
+}
+ 
+function validateDate() {
+  const inputValue = document.getElementById('inputValue').value;
+  const result = isDate(inputValue);
 
-// Do not change the code below.
-const input = prompt("Enter Date.");
-alert(isDate(input));
+  document.getElementById('result').innerText = `Is Valid Date: ${result}`;
+}
